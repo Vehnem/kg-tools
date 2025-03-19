39b03b95e7610e3d65dd9b7c9c5ecfefea623ef3
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-03-19 15:14:47 +0100

    added dockerfiles for airflow with create_docker_images.sh
    - falcon new (not working)
    
    - fixed copy whole folder
    - fixed magellan
    - added .dockerignore
    
    - added falcon (not working), magellan, pyjedai and REL Dockerfiles for Airflow
    
    - added dockerfiles for airflow with create_docker_images.sh

---
+++ _scripts/create_docker_images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BASE_DIR=$(pwd)
+
+for dir in "$BASE_DIR"/*/; do
+
+    DIR_NAME=$(basename "$dir")
+
+    if [[ "$DIR_NAME" == _* ]]; then
+        echo "Skipping directory beginning with '_': $dir"
+        continue
+    fi
+
+    if [[ -f "$dir/DockerfileAirflow" ]]; then
+        echo "Dockerfile found in: $dir"
+
+        IMAGE_NAME="kg-tools/${DIR_NAME,,}_image"
+        echo "Create Image: $IMAGE_NAME"
+        docker build -t "$IMAGE_NAME" -f "$dir/DockerfileAirflow" "$BASE_DIR"
+
+    else
+        echo "No Dockerfile found in: $dir, skip..."
+    fi
+done
