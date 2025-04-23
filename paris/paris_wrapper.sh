6696feab18f80249fac1e508b8e26397cc2b64b6
Marvin <vehnem@yahoo.de>
2025-04-23 16:20:19 +0200

    paris_wrapper

---
+++ paris/paris_wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# bash paris_wrapper.sh path/to/infile1 path/to/infile2 path/to/outputfolder
+
+IN1=$1
+IN2=$2
+OUT=$3
+
+CONF=settings.ini
+
+echo "endIteration=20" > $CONF
+echo "factstore1=$IN1" >> $CONF
+echo "factstore2=$IN2" >> $CONF
+echo "resultTSV=$OUT" >> $CONF
+echo "home=$(pwd)" >> $CONF
+
+mkdir -p $OUT
+echo $OUT | java -jar *.jar settings.ini
