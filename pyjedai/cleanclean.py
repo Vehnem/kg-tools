2cc1ad3717dfc1416fe12f7322816159c07182aa
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-04-25 15:01:02 +0200

    updated pyjedai

--- pyjedai/cleanclean.py
+++ pyjedai/cleanclean.py
@@ -25,18 +25,18 @@ def main():
 
     data_cleaning.clean(data)
 
-    if args.attr1 and args.attr2:
+    if not (args.attr1 and args.attr2):
+        attributes1 = []
+        attributes2 = []
+    else:
         attributes1 = [attr.strip() for attr in args.attr1.split(',')]
         attributes2 = [attr.strip() for attr in args.attr2.split(',')]
 
-        blocks = token_blocking.block(data, attributes1, attributes2)
-        cleaned_blocks = block_purging.purge(blocks, data)
-        filtered_blocks = block_cleaning.clean(cleaned_blocks, data)
-        candidate_pairs_blocks = weighted_edge_pruning.clean(filtered_blocks, data)
-    else:
-        id1 = data.dataset_1[data.id_column_name_1].tolist()
-        id2 = data.dataset_2[data.id_column_name_2].tolist()
-        candidate_pairs_blocks = list(product(id1, id2))
+    blocks = token_blocking.block(data, attributes1, attributes2)
+    cleaned_blocks = block_purging.purge(blocks, data)
+    filtered_blocks = block_cleaning.clean(cleaned_blocks, data)
+    candidate_pairs_blocks = weighted_edge_pruning.clean(filtered_blocks, data)
+
 
     # Matching
     pairs_graph = entity_matching.match(candidate_pairs_blocks, data)
