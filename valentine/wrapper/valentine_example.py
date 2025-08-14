e18a7da7f0022f60937be5909d5480b85c2ce481
theodebian-laptop <gg46ixav@studserv.uni-leipzig.de>
2025-08-14 14:16:24 +0200

    updated valentine_example.py

--- valentine/wrapper/valentine_example.py
+++ valentine/wrapper/valentine_example.py
@@ -34,26 +34,6 @@ def main():
     one_to_one_matches = matches.one_to_one()
     pp.pprint(one_to_one_matches)
 
-    # If ground truth available, calculate metrics
-    ground_truth = [('Cited by', 'Cited by'),
-                    ('Authors', 'Authors'),
-                    ('EID', 'EID')]
-
-    metrics = matches.get_metrics(ground_truth)
-
-    print("\nAccording to the ground truth:")
-    pp.pprint(ground_truth)
-
-    print("\nThese are the scores of the default metrics for the matcher:")
-    pp.pprint(metrics)
-
-    print("\nYou can also get specific metric scores:")
-    specific_metrics = matches.get_metrics(ground_truth, metrics={
-        PrecisionTopNPercent(n=80),
-        F1Score()
-    })
-    pp.pprint(specific_metrics)
-
     print("\nThe MatcherResults object is a dict and can be treated such:")
     for match in matches:
         print(f"{str(match): <60} {matches[match]}")
